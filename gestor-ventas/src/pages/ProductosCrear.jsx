01afec2270a1f7a8ddde438cf79d6fe19f55fcaa
Ricardo Corredor <corredorlopez@gmail.com>
2021-10-02 17:51:23 -0500

    Modificacion en detalles de los estilos

--- gestor-ventas/src/pages/ProductosCrear.jsx
+++ gestor-ventas/src/pages/ProductosCrear.jsx
@@ -4,7 +4,7 @@ const ProductosCrear = () => {
     return (
         <>
             
-            <div className="mt-10 sm:mt-0">
+            <div className="mt-10 sm:mt-0 h-full w-3/4">
                 <div className="md:grid md:grid-cols-3 md:gap-6">
                     <div className="mt-5 md:mt-0 md:col-span-2">
                         <form action="#" method="POST">
@@ -13,52 +13,52 @@ const ProductosCrear = () => {
                                 <div className="px-4 py-5 bg-white sm:p-6">
                                     <div className="grid grid-cols-6 gap-6">
                                         <div className="col-span-6 sm:col-span-3">
-                                            <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                                            <label htmlFor="idproducto" className="block text-sm font-medium text-gray-700">
                                                 Id. Producto
                                             </label>
                                             <input
                                                 type="number"
-                                                name="first-name"
-                                                id="first-name"
+                                                name="idproducto"
+                                                id="idproducto"
                                                 autoComplete="given-name"
                                                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                             />
                                         </div><br/>
 
                                         <div className="col-span-6 sm:col-span-3">
-                                            <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                                            <label htmlFor="descripcion" className="block text-sm font-medium text-gray-700">
                                                 Descripcion Producto
                                             </label>
                                             <input
                                                 type="text"
-                                                name="last-name"
-                                                id="last-name"
+                                                name="descricpcion"
+                                                id="descripcion"
                                                 autoComplete="family-name"
                                                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                             />
                                         </div>
 
                                         <div className="col-span-6 sm:col-span-4">
-                                            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                                            <label htmlFor="valor-unitario" className="block text-sm font-medium text-gray-700">
                                                 Valor Unitario
                                             </label>
                                             <input
                                                 type="number"
-                                                name="email-address"
-                                                id="email-address"
-                                                autoComplete="email"
+                                                name="valor-unitario"
+                                                id="valor-unitario"
+                                                autoComplete="valoruni"
                                                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                             />
                                         </div>
 
                                         <div className="col-span-6 sm:col-span-3">
-                                            <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                                            <label htmlFor="estado" className="block text-sm font-medium text-gray-700">
                                                 Estado Producto
                                             </label>
                                             <select
-                                                id="producto"
-                                                name="producto"
-                                                autoComplete="country"
+                                                id="estado"
+                                                name="estado"
+                                                autoComplete="estado"
                                                 className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                             >
                                                 <option>Disponible</option>
@@ -73,7 +73,7 @@ const ProductosCrear = () => {
                                         type="submit"
                                         className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                     >
-                                        Save
+                                        Guardar
                                     </button>
                                 </div>
                             </div>

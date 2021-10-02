33c4da8b8db90fb5173ce7f8ead01ac9915f45f7
Ricardo Corredor <corredorlopez@gmail.com>
2021-10-02 17:03:17 -0500

    Creacion de interfaz para registro de productos

--- gestor-ventas/src/pages/ProductosCrear.jsx
+++ gestor-ventas/src/pages/ProductosCrear.jsx
@@ -2,10 +2,88 @@ import React from 'react'
 
 const ProductosCrear = () => {
     return (
-        <div>
-            Creación de productos
-        </div>
+        <>
+            
+            <div className="mt-10 sm:mt-0">
+                <div className="md:grid md:grid-cols-3 md:gap-6">
+                    <div className="mt-5 md:mt-0 md:col-span-2">
+                        <form action="#" method="POST">
+                            <h2 className="text-lg font-medium leading-6 text-gray-900">Registro de Productos</h2><br/>
+                            <div className="shadow overflow-hidden sm:rounded-md">
+                                <div className="px-4 py-5 bg-white sm:p-6">
+                                    <div className="grid grid-cols-6 gap-6">
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                                                Id. Producto
+                                            </label>
+                                            <input
+                                                type="number"
+                                                name="first-name"
+                                                id="first-name"
+                                                autoComplete="given-name"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+                                        </div><br/>
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                                                Descripcion Producto
+                                            </label>
+                                            <input
+                                                type="text"
+                                                name="last-name"
+                                                id="last-name"
+                                                autoComplete="family-name"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+                                        </div>
+
+                                        <div className="col-span-6 sm:col-span-4">
+                                            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                                                Valor Unitario
+                                            </label>
+                                            <input
+                                                type="number"
+                                                name="email-address"
+                                                id="email-address"
+                                                autoComplete="email"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+                                        </div>
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                                                Estado Producto
+                                            </label>
+                                            <select
+                                                id="producto"
+                                                name="producto"
+                                                autoComplete="country"
+                                                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                            >
+                                                <option>Disponible</option>
+                                                <option>No Disponible</option>
+                                            </select>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                    <button
+                                        type="submit"
+                                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    >
+                                        Save
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+                     
+        </>
     )
 }
-
 export default ProductosCrear;

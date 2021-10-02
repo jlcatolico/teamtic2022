f7e16410c219a644a3b563b792317a9ee4c65337
mariafmedinae <mariafmedinae@gmail.com>
2021-10-02 16:00:48 -0500

    Avances Listar Usuarios

--- gestor-ventas/src/layouts/PrivateLayout.jsx
+++ gestor-ventas/src/layouts/PrivateLayout.jsx
@@ -7,7 +7,7 @@ const PrivateLayout = ({ children }) => {
   return (
     <div className="flex w-screen h-screen">
       <Sidebar />
-      <div className='flex flex-col h-screen w-screen justify-between'>
+      <div className="flex flex-col h-screen w-screen justify-between">
         <Navbar/>
         <main className="flex w-full h-full overflow-y-scroll p-2">{children}</main>
         <Footer />

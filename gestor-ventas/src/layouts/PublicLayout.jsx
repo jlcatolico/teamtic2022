8a8a76e8712b8da8d22763a7d4b884cff4186ed5
Dan0722 <90528330+Dan0722@users.noreply.github.com>
2021-10-03 16:53:37 -0500

    Modificacion navbar, layout NavbarPublic OK

--- gestor-ventas/src/layouts/PublicLayout.jsx
+++ gestor-ventas/src/layouts/PublicLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from 'components/Footer';
-import Navbar from 'components/Navbar';
+import Navbar from 'components/NavbarPublic';
 
 const PublicLayout = ({children}) => {
     return (

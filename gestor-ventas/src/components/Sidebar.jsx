fe907d0ae737450f29f31787f71b02bbde98f585
Lorenzo Catolico <lorenzo.catolico@gmail.com>
2021-10-14 19:41:26 -0500

    actualizcion sidebar, creacion proyecto api

--- gestor-ventas/src/components/Sidebar.jsx
+++ gestor-ventas/src/components/Sidebar.jsx
@@ -1,62 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBox, faBoxes, faCashRegister, faHome, faSearchDollar, faUsers } from '@fortawesome/free-solid-svg-icons';
+import Logo from 'Media/Toys.png';
 
 const Sidebar = () => {
   return (
-    <nav className="w-1/5 border border-gray-700 bg-gray-700 h-full p-2 justify">
-      <ul className="flex flex-col h-full justify-around">
-        <li className="px-3 m-3">
-          <Link to="/Inside">
-            <a className="text-gray-700 p-2 rounded bg-yellow-300 flex items-stretch">
-              <FontAwesomeIcon icon={faHome} className="m-1" />
-              Home
-            </a>
-          </Link>
-        </li>
-        <li className="px-3 m-3">
-          <Link to="/ProductosListar">
-            <a className="text-white p-2 rounded bg-blue-500 flex items-stretch">
-              <FontAwesomeIcon icon={faBoxes} className='m-1'/>
-              Productos
-            </a>
-          </Link>
-        </li>
-        <li className="px-3 m-3">
-          <Link to="/ProductosCrear">
-            <a className="text-white p-2 rounded bg-blue-400 flex items-stretch">
-              <FontAwesomeIcon icon={faBox} className='m-1'/>
-              Crear producto
-            </a>
-          </Link>
-        </li>
-        <li className="px-3 m-3">
-          <Link to="/VentasListar">
-            <a className="text-white p-2 rounded bg-red-500 flex items-stretch">
-              <FontAwesomeIcon icon={faSearchDollar} className='m-1'/>
-              Ventas
-            </a>
-          </Link>
-        </li>
-        <li className="px-3 m-3">
-          <Link to="/VentasCrear">
-            <a className="text-white p-2 rounded bg-red-400 flex items-stretch">
-              <FontAwesomeIcon icon={faCashRegister} className='m-1'/>
-              Crear venta
-            </a>
-          </Link>
-        </li>
-        <li className="px-3 m-3">
-          <Link to="/UsuariosListar">
-            <a className="text-white p-2 rounded bg-green-600 flex items-stretch">
-              <FontAwesomeIcon icon={faUsers} className='m-1'/>
-              Usuarios
-            </a>
-          </Link>
-        </li>
+
+    <nav className="w-72 border border-gray-300 bg-white p-2 justify">
+      <div className="px-4">
+        <img alt='...' src={Logo} className='max-w-200-px' />
+      </div>
+      <ul className="flex flex-col justify-around">
+        <BotonSideBar nombre="Home" ruta="/Inside" icono="fas fa-home" color="bg-yellow-300" />
+        <BotonSideBar nombre="Productos" ruta="/ProductosListar" icono="fas fa-gifts" color="bg-blue-500" />
+        <BotonSideBar nombre="Crear Producto" ruta="/ProductosCrear" icono="fas fa-gift" color="bg-blue-400" />
+        <BotonSideBar nombre="Ventas" ruta="/VentasListar" icono="far fa-list-alt" color="bg-red-500" />
+        <BotonSideBar nombre="Crear venta" ruta="/VentasCrear" icono="fas fa-cart-arrow-down" color="bg-red-400" />
+        <BotonSideBar nombre="Usuarios" ruta="/UsuariosListar" icono="fas fa-users" color="bg-green-500" />
       </ul>
     </nav>
+  );
+};
+
+
+const BotonSideBar = ({ nombre, ruta, icono, color }) => {
+  return (
+
+    <Link to={ruta}>
+      <li className={`px-1 m-4 text-white p-2 rounded ${color} flex items-center`}>
+        <i className={`${icono} w-6`} />
+        {nombre}
+      </li>
+    </Link>
+
   );
 };
 

d0a862ee110d628c443f24280aa200236216d09e
Valentina Arbelaez <varbelaez@gmail.com>
2021-10-17 21:33:11 -0500

    se soluciona useLocation en useActiveRoute.js

--- gestor-ventas/src/components/Sidebar.jsx
+++ gestor-ventas/src/components/Sidebar.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import Logo from 'Media/Toys.png';
-import useActiveRouter from 'hooks/useActiveRouter';
+import useActiveRoute from 'hooks/useActiveRoute';
 
 const Sidebar = () => {
 	return (
-		<nav className='hidden md:flex md:w-50 bg-gray-600 p-2 flex flex-col'>
+		<nav className='hidden md:flex md:w-50 bg-gray-100 border border-gray-300 p-2 flex flex-col'>
 			<div className='px-4'>
 				<img alt='...' src={Logo} className='max-w-200-px my-2' />
 			</div>
 			<ul className='flex flex-col'>
-				<BotonSideBar nombre='Home' ruta='/Inside' icono='fas fa-home' color='red' />
-				<BotonSideBar nombre='Productos' ruta='/Productos' icono='fas fa-gifts' color='red' />
-				<BotonSideBar nombre='Ventas' ruta='/VentasListar' icono='far fa-list-alt' color='red' />
-				<BotonSideBar nombre='Usuarios' ruta='/Usuarios' icono='fas fa-users' color='red' />
+				<BotonSideBar nombre='Home' ruta='/Inside' icono='fas fa-home' />
+				<BotonSideBar nombre='Productos' ruta='/Productos' icono='fas fa-gifts' />
+				<BotonSideBar nombre='Ventas' ruta='/VentasListar' icono='far fa-list-alt' />
+				<BotonSideBar nombre='Usuarios' ruta='/Usuarios' icono='fas fa-users' />
 			</ul>
 		</nav>
 	);
 };
 
-const BotonSideBar = ({nombre, ruta, icono, color}) => {
-	const isActive = useActiveRouter(ruta);
+const BotonSideBar = ({nombre, ruta, icono}) => {
+	const isActive = useActiveRoute(ruta);
+	const color = 'pink';
+
 	return (
 		<Link to={ruta}>
-			<li className={`px-1 m-2 text-white p-2 rounded-lg bg-${color}-500 hover:bg-${color}-900 flex items-center`}>
+			<li className={`px-1 m-2 text-white p-2 rounded-lg bg-${isActive ? 'yellow' : 'gray'}-400 hover:bg-${color}-600 flex items-center`}>
 				<i className={`${icono} w-6`} />
 				{nombre}
 			</li>

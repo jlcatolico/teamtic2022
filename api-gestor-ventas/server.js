2209ce8e5055bba387a1d968ae7622c05e730567
Valentina Arbelaez <varbelaez@gmail.com>
2021-10-20 21:57:07 -0500

    Se genera controlador y rutas para listar, buscar, actualizar y eliminar usuarios

--- api-gestor-ventas/server.js
+++ api-gestor-ventas/server.js
@@ -3,33 +3,35 @@
 
 //import express modulo
 import Express from "express";
-import dotenv from 'dotenv';
-import { conectarDB} from './db/db.js';
+import dotenv from "dotenv";
+import { conectarDB } from "./db/db.js";
 import rutasProducto from "./views/productos/rutas.js";
+import rutasUsuario from "./views/usuarios/rutas.js";
 
-
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 const app = Express();
+
 app.use(Express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
 app.use(rutasProducto);
+app.use(rutasUsuario);
 
 const main = () => {
-
-    return app.listen(process.env.PORT, () => {
-        console.log(`escuchando puerto ${process.env.PORT}`);
-    });
-
-
-}
+  return app.listen(process.env.PORT, () => {
+    console.log(`escuchando puerto ${process.env.PORT}`);
+  });
+};
 
 conectarDB(main);
